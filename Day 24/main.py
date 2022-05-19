66379946e2e0b7358308e2e6ab323eee13972e4e
Nachop51 <nachoperalta0@gmail.com>
2022-05-19 17:31:17 +0000

    testing day 24

--- Day 24/main.py
+++ Day 24/main.py
@@ -1,3 +1,15 @@
 with open("Input/Names/invited_names.txt") as file:
-    file.read()
+    names = file.readlines()
 file.close()
+
+names = list(map(lambda x: x.strip('\n'), names))
+
+with open("Input/Letters/starting_letter.txt") as file:
+    letter = file.read()
+file.close()
+
+for name in names:
+    name = name.strip(' ')
+    with open(f"Output/ReadyToSend/{name[:]}", "w") as file:
+        file.write(letter.replace("[name]", name))
+    file.close()
