bac31b67d3ef611f078a72b3e79aed4b20d4ff0a
Nacho Peralta <nachoperalta0@gmail.com>
2022-05-19 23:10:58 -0300

    Testing pycharm

--- Day 25 (CSV Data)/main.py
+++ Day 25 (CSV Data)/main.py
@@ -14,4 +14,22 @@
 
 import pandas
 
-pandas.read_csv("weather_data.csv",)
+data = pandas.read_csv("weather_data.csv",)
+# print(data)
+print(type(data))
+print(type(data["temp"]))  # Similar to a list, actually is a "series"
+# print(data["temp"])
+
+data_dict = data.to_dict()
+print(data_dict)
+
+temp_list = data["temp"].to_list()
+print(temp_list)
+
+avg = sum(temp_list) / len(temp_list)
+print(avg)
+
+
+
+
+
