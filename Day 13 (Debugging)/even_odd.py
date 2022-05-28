819df8ec12fbb8526ac14d50e70d64f9c2e7bfaf
Nacho Peralta <nachoperalta0@gmail.com>
2022-05-27 21:28:34 -0300

    uploading previous days

---
+++ Day 13 (Debugging)/even_odd.py
@@ -0,0 +1,11 @@
+# number = int(input("Which number do you want to check?"))
+
+# if number % 2 = 0:
+#     print("This is an even number.")
+# else:
+#     print("This is an odd number.")
+
+# TODO 1
+# UNCOMMENT THE CODE ABOVE
+# TODO 2
+# DEBUG THIS
---
+++ Day 13 (Debugging)/fizzbuzz.py
@@ -0,0 +1,14 @@
+# for number in range(1, 101):
+#       if number % 3 == 0 or number % 5 == 0:
+#     print("FizzBuzz")
+#   if number % 3 == 0:
+#     print("Fizz")
+#   if number % 5 == 0:
+#     print("Buzz")
+#   else:
+#     print([number])
+
+# TODO 1
+# UNCOMMENT THE CODE ABOVE
+# TODO 2
+# DEBUG THIS
