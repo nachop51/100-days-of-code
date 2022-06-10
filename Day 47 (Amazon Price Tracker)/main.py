98df7c6eb09346804278e86b618c1acbd016ddde
Nacho Peralta <nachoperalta0@gmail.com>
2022-06-10 17:42:57 -0300

    Day 47

--- Day 47 (Amazon Price Tracker)/main.py
+++ Day 47 (Amazon Price Tracker)/main.py
@@ -25,7 +25,7 @@ print(price)
 
 if price < BUDGET:
     print("Buy it!")
-    with smtplib.SMTP("smtp.gmail.com") as connection:
+    with smtplib.SMTP("smtp.gmail.com", port=587) as connection:
         connection.starttls()
         connection.login(user=my_email, password=password)
         connection.sendmail(
