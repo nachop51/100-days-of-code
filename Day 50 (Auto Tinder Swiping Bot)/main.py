ba48537d2e8e1b6999d9b54331fa46df3893054e
Nacho Peralta <nachoperalta0@gmail.com>
2022-06-11 22:07:26 -0300

    Day 51

--- Day 50 (Auto Tinder Swiping Bot)/main.py
+++ Day 50 (Auto Tinder Swiping Bot)/main.py
@@ -26,7 +26,7 @@ time.sleep(1)
 facebook = driver.find_element(
     By.XPATH, '//*[@id="t-1181997678"]/div/div/div[1]/div/div/div[3]/span/div[2]/button'
 ).click()
-time.sleep(1)
+time.sleep(2)
 
 # Change the current selenium window
 fb_login_window = driver.window_handles[1]
@@ -34,17 +34,16 @@ driver.switch_to.window(fb_login_window)
 
 email = driver.find_element(
     By.NAME, 'email'
-).send_keys("dashernacho@gmail.com")
+).send_keys("your mail")
 time.sleep(1)
-password = driver.find_element(By.NAME, 'pass').send_keys("esladelparque")
+password = driver.find_element(By.NAME, 'pass').send_keys("your password")
 time.sleep(1)
 log_in = driver.find_element(By.NAME, 'login').click()
 time.sleep(1)
-
 accept_terms = driver.find_element(
     By.XPATH, '//*[@id="mount_0_0_cB"]/div/div/div/div/div/div/div[1]/div[2]/div[2]/div[1]/div/div/div'
 ).click()
-
+time.sleep(10)
 # Get back to the tinder window
 tinder_window = driver.window_handles[0]
 driver.switch_to.window(tinder_window)
